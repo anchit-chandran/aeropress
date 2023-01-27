49851586e8d038c85f492228de3240f03f2d6dc3
Anchit <68898006+anchit-chandran@users.noreply.github.com>
2023-01-27 19:38:34 +0000

    styling changes inc prev recipes

--- aeropressSite/aeropress/forms.py
+++ aeropressSite/aeropress/forms.py
@@ -4,33 +4,33 @@ from django import forms
 class RecipeForm(forms.Form):
 
     coffee_g = forms.IntegerField(min_value=0,
-
                                   step_size=1,
                                   label='Coffee used (g)☕',
+        
                                   widget=forms.NumberInput(
-                                      attrs={'class': 'form-control w-25'}
+                                      attrs={'class': 'form-control form-control-sm'}
                                   ))
     water_brew_ml = forms.IntegerField(min_value=0,
                                        step_size=1,
                                        label='Water used for extraction (mL)💧',
                                        widget=forms.NumberInput(
-                                           attrs={'class': 'form-control w-25'}
+                                           attrs={'class': 'form-control form-control-sm'}
                                        ))
     brew_time_s = forms.IntegerField(label='Extraction time (s)⌛',
                                      widget=forms.NumberInput(
-                                         attrs={'class': 'form-control w-25'}
+                                         attrs={'class': 'form-control form-control-sm'}
                                      ))
     milk_ml = forms.IntegerField(min_value=0,
                                  step_size=1,
                                  label='Milk used (mL)🥛',
                                  widget=forms.NumberInput(
-                                     attrs={'class': 'form-control w-25'}
+                                     attrs={'class': 'form-control form-control-sm'}
                                  ))
     extra_water_ml = forms.IntegerField(min_value=0,
                                         step_size=1,
                                         label='Extra water added (mL)💦',
                                         widget=forms.NumberInput(
-                                            attrs={'class': 'form-control w-25'}
+                                            attrs={'class': 'form-control form-control-sm'}
                                         ))
     nausea = forms.BooleanField(required=False, label='Experienced nausea🤢',
                                         widget=forms.CheckboxInput(
@@ -45,5 +45,5 @@ class RecipeForm(forms.Form):
         step_size=1,
         label='Rating (/10)🌟',
         widget=forms.NumberInput(
-            attrs={'class': 'form-control w-25'}
+            attrs={'class': 'form-control form-control-sm'}
         ))
