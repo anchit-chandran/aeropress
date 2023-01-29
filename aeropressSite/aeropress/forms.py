7cca97ada4cb77e677238482eec3b027f03476af
Anchit <68898006+anchit-chandran@users.noreply.github.com>
2023-01-29 10:56:19 +0000

    added delete function

--- aeropressSite/aeropress/forms.py
+++ aeropressSite/aeropress/forms.py
@@ -6,39 +6,43 @@ class RecipeForm(forms.Form):
     coffee_g = forms.IntegerField(min_value=0,
                                   step_size=1,
                                   label='Coffee used (g)☕',
-        
+
                                   widget=forms.NumberInput(
-                                      attrs={'class': 'form-control form-control-sm'}
+                                      attrs={
+                                          'class': 'form-control form-control-sm'}
                                   ))
     water_brew_ml = forms.IntegerField(min_value=0,
                                        step_size=1,
                                        label='Water used for extraction (mL)💧',
                                        widget=forms.NumberInput(
-                                           attrs={'class': 'form-control form-control-sm'}
+                                           attrs={
+                                               'class': 'form-control form-control-sm'}
                                        ))
     brew_time_s = forms.IntegerField(label='Extraction time (s)⌛',
                                      widget=forms.NumberInput(
-                                         attrs={'class': 'form-control form-control-sm'}
+                                         attrs={
+                                             'class': 'form-control form-control-sm'}
                                      ))
     milk_ml = forms.IntegerField(min_value=0,
                                  step_size=1,
                                  label='Milk used (mL)🥛',
                                  widget=forms.NumberInput(
-                                     attrs={'class': 'form-control form-control-sm'}
+                                     attrs={
+                                         'class': 'form-control form-control-sm'}
                                  ))
     extra_water_ml = forms.IntegerField(min_value=0,
                                         step_size=1,
                                         label='Extra water added (mL)💦',
                                         widget=forms.NumberInput(
-                                            attrs={'class': 'form-control form-control-sm'}
+                                            attrs={
+                                                'class': 'form-control form-control-sm'}
                                         ))
     nausea = forms.BooleanField(required=False, label='Experienced nausea🤢',
-                                        widget=forms.CheckboxInput(
-                                            attrs=
-                                            {'class': 'form-check-input',
-                                            'role' : 'switch'
-                                            }
-                                        ))
+                                widget=forms.CheckboxInput(
+                                    attrs={'class': 'form-check-input',
+                                           'role': 'switch'
+                                           }
+                                ))
     rating = forms.IntegerField(
         min_value=0,
         max_value=10,
--- aeropressSite/aeropress/views.py
+++ aeropressSite/aeropress/views.py
@@ -10,28 +10,40 @@ from .forms import RecipeForm
 def home(response):
 
     if response.method == 'POST':
-        print(response.POST)
-        form = RecipeForm(response.POST)
-        if form.is_valid():
-            f = Recipe(
-                coffee_g=form.cleaned_data['coffee_g'],
-                water_brew_ml=form.cleaned_data['water_brew_ml'],
-                brew_time_s=form.cleaned_data['brew_time_s'],
-                milk_ml=form.cleaned_data['milk_ml'],
-                extra_water_ml=form.cleaned_data['extra_water_ml'],
-                nausea=form.cleaned_data['nausea'],
-                rating=form.cleaned_data['rating'],
-            )
-            f.save()
+        
+        if response.POST.get('add'):
+            print('adding form')
+            form = RecipeForm(response.POST)
+            if form.is_valid():
+                f = Recipe(
+                    coffee_g=form.cleaned_data['coffee_g'],
+                    water_brew_ml=form.cleaned_data['water_brew_ml'],
+                    brew_time_s=form.cleaned_data['brew_time_s'],
+                    milk_ml=form.cleaned_data['milk_ml'],
+                    extra_water_ml=form.cleaned_data['extra_water_ml'],
+                    nausea=form.cleaned_data['nausea'],
+                    rating=form.cleaned_data['rating'],
+                )
+                f.save()
+                return HttpResponseRedirect('/')
+        elif response.POST.get('delete'):
+            print("SUBMIT POST",response.POST)
+            print('delete')
+            for item in Recipe.objects.all():
+                if response.POST.get(f's{item.id}'):
+                    print(f'delete item {item}')
+                    Recipe.objects.filter(id=item.id).delete()
             return HttpResponseRedirect('/')
-    else:
-        form = RecipeForm(initial={
-            'coffee_g': 0,
-            'water_brew_ml' : 0,
-            'brew_time_s' : 0,
-            'milk_ml' : 0,
-            'extra_water_ml' : 0,
-            'rating' : 0,
-        })
-    rs = Recipe.objects.all()
-    return render(response, 'aeropress/base.html', {'rs': rs, 'form': form})
+    
+    form = RecipeForm(initial={
+        'coffee_g': 0,
+        'water_brew_ml' : 0,
+        'brew_time_s' : 0,
+        'milk_ml' : 0,
+        'extra_water_ml' : 0,
+        'rating' : 0,
+    })
+
+
+    rs = Recipe.objects.all()[::-1]
+    return render(response, 'aeropress/base.html', {'rs': rs, 'form': form,})
